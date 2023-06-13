3b6cf3a9512708ce590b53c9085a1d196a095870
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-13 23:55:18 +0300

    app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
-<template>
+<!-- <template>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+</template> -->
+<template>
+  <AppHeader />
+  <Container />
 </template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    Container
+}
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Container.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <RouterView />
+    </div>
+</template>
+<style scoped>
+.container{
+    padding: 64px;
+}
+</style>
