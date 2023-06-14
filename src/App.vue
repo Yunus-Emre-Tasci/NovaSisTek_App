8939183f81ea7c5bf237a726fcbde5bd9870a28b
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-14 08:45:05 +0300

    reset.css

--- src/App.vue
+++ src/App.vue
@@ -6,32 +6,41 @@
   <router-view/>
 </template> -->
 <template>
-  <AppHeader />
+  <!-- <AppHeader /> -->
   <Container />
 </template>
 
 <script>
-
+import Container from "./components/Container.vue"
 
 export default {
   name: 'App',
   components: {
-    AppHeader,
+    // AppHeader,
     Container
 }
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: 'Poppins', sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-nav {
+/* nav {
   padding: 30px;
 
   a {
@@ -42,5 +51,5 @@ nav {
       color: #42b983;
     }
   }
-}
+} */
 </style>
