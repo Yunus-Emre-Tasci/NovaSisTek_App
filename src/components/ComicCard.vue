4cc05a083655e4eca2de913b2df27ae136161903
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-14 13:47:01 +0300

    route-detail

--- src/components/ComicCard.vue
+++ src/components/ComicCard.vue
@@ -1,13 +1,21 @@
 <template>
-  <div> {{item.title}} </div>
+  <div  @click="goToDetail"> {{item.title}} </div>
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+
 export default {
   props: ['item'],
   name: 'ComicCard',
   setup(){
-    
+    const router = useRouter();
+    const goToDetail = () => {
+      router.push('/detail');
+    };
+    return {
+      goToDetail,
+    };
   }
 
 }
